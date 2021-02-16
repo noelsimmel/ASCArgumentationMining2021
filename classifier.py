b4e843d3aeb2600f528698b11fc1be2816a565b4
noelsimmel <noelsimmel@protonmail.com>
2021-02-16 13:29:14 +0100

    Add baseline classifier linear SVM

--- classifier.py
+++ classifier.py
@@ -1,6 +1,10 @@
+from collections import namedtuple
 import logging
 import pandas as pd
+from sklearn import metrics
 from sklearn import svm
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import Perceptron
 from sklearn.model_selection import train_test_split
 
 # Logging configuration
@@ -12,94 +16,105 @@ file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
 class ASCClassifier:
-    def __init__(self, fn):
-        """
-        Constructor
-        """
+    def __init__(self):
+        ""
 
-        self.data = self._read_data(fn)
+        model_attributes = ['clf', 'accuracy', 'f1']
+        self.model = namedtuple('Model', model_attributes)
 
-    def split_dataset(self, df):
-        """
-        Split data into training and test sets (80:20). 
-        Save datasets to tsv files.
+    def train(self, train_file, test_model=False):
+        ""
 
-        - df (dataframe): [Preprocessed] data
+        train_data = self._read_file(train_file)
+        test_data = pd.DataFrame()
+        # Split dataset if classifier should be tested
+        if test_model:
+            # seed=21 shows atheism=[0,-1,1,0,0] at .head()
+            train_data, test_data = self._split_dataset(train_data, seed=21)
+        logger.info(f"Training start")
+        
+        # Baseline classifier: Linear SVM 
+        baseline_clf = self.train_baseline_clf(train_data, test_data)
+        print(baseline_clf)
+        
+        # Extract features
+        # features = self._extract_features(train_data)
+        # data = pd.merge(train_data, features, on="id")
+        # logger.info(f"Training finished: df shape {data.shape}, {features.shape[1]} features")
 
-        Return training and test sets as dataframes.
-        """
+        # TODO: Build model
 
-        # Atheism value counts: 
-        # none 220, favor 147, against 348, mean -0.281119 (n=715)
-        # Split in train and test sets (80:20)
-        train, test = train_test_split(df, test_size=0.2, stratify=df.atheism)
-        train.to_csv('data/dataset_training.tsv', sep="\t")
-        test.to_csv('data/dataset_test.tsv', sep="\t")
-        logger.info("Saved datasets to data folder in tsv format")
+        # if test_model:
+        #     self.evaluate(test_data, clf)
+        
+    def train_baseline_clf(self, train_data, test_data):
+        ""
+        
+        # Class label must be at column index 2
+        labels = train_data.columns[2]
+        train_text = train_data.text
+        y_train = train_data[labels]
 
-    def train(self):
-        """
-        """
+        # Bag of words
+        vectorizer = TfidfVectorizer()
+        X_train = vectorizer.fit_transform(train_text)
+        
+        # Linear SVM
+        # Balanced class weight gave better accuracy than unbalanced (+0.035)
+        clf = svm.SVC(kernel="linear", class_weight="balanced")
+        clf.fit(X_train, y_train)
+        logger.info(f"Created baseline classifier {clf}")
+        
+        return self.evaluate(clf, test_data, vectorizer)
 
-        # Daten einlesen, wenn nötig
-        if type(input_data) == str: input_data = self._preprocess(input_data)
-        logger.info(f"Beginn Training ({input_data.shape[0]} Zeilen)")
-        # Features extrahieren und an den DF anhängen
-        features_only = self._extract_features(input_data)
-        features = pd.merge(input_data, features_only, on=['user_id'])
-        logger.info(f"Ende Training: {features.shape[0]} Zeilen, {features.shape[1]} Spalten")
-
-        # Features aggregieren, d.h. für jede Klasse über alle Instanzen mitteln
-        agg_features = self._aggregate_features(features)
-        # Aggregierte Features in tsv-Datei schreiben
-        # Habe tsv statt json gewählt, weil es für Menschen besser lesbar ist
-        agg_features.to_csv(output_filename, sep='\t')
-        logger.info(f"Aggregierte Features in {output_filename} geschrieben")
-        return agg_features
-    
     def predict(self):
-        """
-        """
+        ""
 
         pass
 
-    def evaluate(self):
-        """
-        """
+    def evaluate(self, clf, test_data, vectorizer=None):
+        ""
 
-        pass
+        # If no test data supplied, return
+        if test_data.empty:
+            return self.model(clf, None, None)
+        
+        y_test = test_data[test_data.columns[2]]
+        X_test = vectorizer.transform(test_data.text)
+        pred = clf.predict(X_test)
+        accuracy = metrics.accuracy_score(y_test, pred)
+        f1 = metrics.f1_score(y_test, pred, average="micro")
+        logger.info(f"Baseline classifier {clf}: Accuracy {accuracy:.3f}, micro f1 {f1:.3f}")
+        return self.model(clf, accuracy, f1)
 
-    def _read_data(self, fn):
-        """
-        Read data from tab-separated txt/csv/tsv file and preprocess.
+    def _read_file(self, fn):
+        ""
 
-        - fn (str): Path to [preprocessed] corpus file
-
-        Return dataframe
-        """
-
-        df = pd.read_csv(fn, sep="\t")
-        # Replace columns for easier access
-        df.columns = ["id", "text", "atheism", "secularism", "religious_freedom", "freethinking", 
-                    "no_evidence", "supernatural", "christianity", "afterlife", "usa", "islam",
-                    "conservatism", "same_sex_marriage"]
-        # Represent stances as ints 0, 1, -1
-        df.fillna(0, inplace=True)
-        df.replace(["none", "favor", "against"], [0, 1, -1], inplace=True)
+        df = pd.read_table(fn)
         logger.info(f"Read data from {fn}: {df.shape} dataframe")
-        return df
+        # Drop all columns except id, text, atheism stance
+        return df[["id", "text", "atheism"]]
 
-    def _extract_features(self):
-        """
-        """
+    def _split_dataset(self, df, seed=None):
+        ""
 
+        # Split in train and test sets (80:20)
+        # FIXME: Stratify by last column = class
+        train, test = train_test_split(df, test_size=0.2, random_state=seed,
+                                       stratify=df[df.columns[-1]])
+        return train, test
+    
+    def _extract_features(self, df):
+        ""
+
+        logger.info(f"Feature extraction start: {len(df)} instances")
         pass
-
+    
 
 if __name__ == '__main__':
-    pd.set_option('display.max_columns', None)
-    clf = MBTIClassifier()
-    f = 'data/corpus_preprocessed'
-    clf.split_dataset(f)
+    # pd.set_option('display.max_columns', None)
+    f = 'data/corpus.txt'
+    clf = ASCClassifier()
+    clf.train(f, test_model=True)
     # clf.train('data/dataset_training.json', 'model.tsv')
     # clf.evaluate('data/dataset_validation.json', 'model.tsv')
