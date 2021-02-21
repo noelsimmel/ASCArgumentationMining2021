98eaf1871f03b500981407f48367234c638da008
noelsimmel <noelsimmel@protonmail.com>
2021-02-21 14:56:14 +0100

    Add try/except for untokenized/tokenized input

--- transformers.py
+++ transformers.py
@@ -5,18 +5,23 @@ class AverageWordLengthExtractor(BaseEstimator, TransformerMixin):
     ""
 
     def fit(self, data, y=None):
-        ""
-        
         return self
 
     def transform(self, data, y=None):
-        ""
+        # Uncomment for pandas version:
+        # data = data["text"].apply(self.average_word_length)
         
-        # data = data[1].apply(self.average_word_length)
         return self.average_word_length(data)
     
-    def average_word_length(self, tweets):
+    def average_word_length(self, data):
         ""
         
-        return [[mean([len(token) for token in t.split()])] for t in tweets]
-        # return [np.mean([len(token) for token in tweets.split()])]
+        # Uncomment for pandas version:
+        # return [np.mean([len(token) for token in data.split()])]
+        
+        # For untokenized text
+        try:
+            return [[mean([len(token) for token in doc.split()])] for doc in data]
+        # For tokenized text
+        except AttributeError:
+            return [[mean([len(token) for token in doc])] for doc in data]
