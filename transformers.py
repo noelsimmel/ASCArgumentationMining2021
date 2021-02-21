e4561571bc3c62ef6d7f343c06b9da35dc21eea6
noelsimmel <noelsimmel@protonmail.com>
2021-02-21 21:04:44 +0100

    Enable named entity recognition

--- transformers.py
+++ transformers.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from nltk import ne_chunk, pos_tag
 from numpy import mean
 from sklearn.base import BaseEstimator, TransformerMixin
 
@@ -35,3 +36,23 @@ class AverageWordLengthExtractor(BaseCustomTransformer):
         # For tokenized text
         except AttributeError:
             return [[mean([len(token) for token in doc])] for doc in data]
+        
+class NamedEntityExtractor(BaseCustomTransformer):
+    ""
+    
+    def make_feature(self, data):
+        ""
+        
+        # POS tagging
+        # Exclude masks as they confuse the parser
+        masks = {"MENTION", "HASHTAG", "BIBLEQUOTE", "QURANQUOTE"}
+        data = [[t for t in doc if t not in masks] for doc in data]
+        tagged = [pos_tag(doc) for doc in data]
+        # NER using nltk
+        chunked = [ne_chunk(doc) for doc in tagged]
+        # Quick and dirty counting named entities:
+        # Flatten list (= syntax tree) and count leftover tuples, 
+        # since NEs are the innermost branches
+        # Based on https://stackoverflow.com/a/952952
+        flattened = [[item for sublist in tree for item in sublist] for tree in chunked]
+        return [[sum(type(t) == tuple for t in doc)] for doc in flattened]
